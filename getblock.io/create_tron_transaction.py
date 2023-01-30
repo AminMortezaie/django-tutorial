1ed76e722360159c4080e53eb59bdf48a893bb8a
aminmortezaie <mohammadmortezaie@gmail.com>
2023-01-30 19:38:39 +0330

    Feat: Use another method to generate private key.

--- getblock.io/create_tron_transaction.py
+++ getblock.io/create_tron_transaction.py
@@ -1,52 +1,52 @@
 import requests
 import json
-from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
+from mnemonic import Mnemonic
 
-# Generate a private key from a mnemonic phrase
+api_key = "382d1202-34e8-4b4f-b458-f37df1bff346"
+
+
 mnemonic = "YOUR-MEMONIC-CODE"
+
 
 # Set the recipient address
 recipient_address = 'TNo4hakGCPWdPCAejiSENYsDZpccoGF2Xr'
+
 # Set the amount to be transferred
 amount = 1
+
 # Set the endpoint URL
-endpoint_url = 'https://api.trongrid.io/wallet/easytransfer'
+endpoint_url = 'https://trx.getblock.io/wallet/easytransfer'
 
 # Set the headers
 headers = {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'x-api-key': api_key
 }
 
-private_key = ec.generate_private_key(ec.SECP256K1(), default_backend())
 
-# Serialize the private key to a string
-private_key_bytes = private_key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.PKCS8,
-    encryption_algorithm=serialization.NoEncryption()
-)
+# Generate a private key from a mnemonic phrase
+def generate_private_key_from_mnemonic(mnemonic):
+    # Generate seed
+    seed = Mnemonic.to_seed(mnemonic)
 
-# Print the private key as a hexadecimal string
-private_key_hex = private_key_bytes.hex()
-# Convert the private key to a JSON string
-private_key = json.dumps({
-    "private_key": private_key_bytes.decode()
-})
+    # Create private key from seed
+    private_key = seed[:32]
+    private_key_hex = private_key.hex()
 
-# Print the JSON string
-print("Private Key (JSON):", private_key)
+    # Print the JSON string
+    print("Private Key (JSON):", private_key_hex)
+    return private_key_hex
+
 
 # Set the payload
 payload = {
-    'privateKey': private_key,
+    'privateKey': generate_private_key_from_mnemonic(mnemonic),
     'toAddress': recipient_address,
     'amount': amount
 }
 
 # Send the POST request
 response = requests.post(endpoint_url, headers=headers, data=json.dumps(payload))
+print(response.content)
 
-# Print the response
-print(response.text)
+
