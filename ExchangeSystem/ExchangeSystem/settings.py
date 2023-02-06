acaef9dbb3ea64b50f91e6eb7c830376e956bbb6
mortezaei <mohammadmortezaie@gmail.com>
2023-02-06 20:36:42 +0330

    Fix: .gitignore updated.

--- ExchangeSystem/ExchangeSystem/settings.py
+++ ExchangeSystem/ExchangeSystem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(6h_3xobn05-0f5k34!*a7ucd653xfz0*_#f2dregns#=6jw02'
+SECRET_KEY = 'YOUR-SECRET-CODE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
