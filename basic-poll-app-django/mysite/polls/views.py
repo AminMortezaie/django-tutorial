b526e8d08380bba8af4dfba632b52ba9ee9a6ced
mortezaei <mohammadmortezaie@gmail.com>
2023-02-08 16:17:31 +0330

    Fix: Update.

--- basic-poll-app-django/mysite/polls/views.py
+++ basic-poll-app-django/mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
