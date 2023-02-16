0484d0d190887b33718ca8d140002bd9f3313f8e
mortezaei <mohammadmortezaie@gmail.com>
2023-02-16 13:03:19 +0330

    Fix: register other models in admin.

--- ExchangeSystem/cosmos/transfer/admin.py
+++ ExchangeSystem/cosmos/transfer/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import SenderWallet
+from .models import ReceiverWallet
+from .models import CreateTransaction
+
 
 admin.site.register(SenderWallet)
+admin.site.register(ReceiverWallet)
+admin.site.register(CreateTransaction)
