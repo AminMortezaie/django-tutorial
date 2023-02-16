8ccffe7ce1a3e065a3943456a99ce3a540b28dd4
mortezaei <mohammadmortezaie@gmail.com>
2023-02-16 14:16:15 +0330

    Fix: add swagger.

--- ExchangeSystem/cosmos/cosmos/urls.py
+++ ExchangeSystem/cosmos/cosmos/urls.py
@@ -16,12 +16,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from transfer import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Transfer API List",
+        default_version='v1',
+        description="Transfer API List",
+    ),
+    public=True,
+    permission_classes=([permissions.AllowAny]),
+)
 
 urlpatterns = [
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),  # <-- Here
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/sender-wallets/', views.SenderWalletList.as_view()),
     path('api/sender-wallet/<int:pk>/', views.SenderWalletObject.as_view()),
     path('api/receiver-wallets/', views.ReceiverWalletList.as_view()),
     path('api/receiver-wallet/<int:pk>/', views.ReceiverWalletObject.as_view()),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api/create-transactions/', views.CreateTransactionsList.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
 ]
