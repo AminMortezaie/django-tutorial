4ba0228a590924ce0f11a1cc64e08e5aa68bb725
mortezaei <mohammadmortezaie@gmail.com>
2023-02-17 18:20:35 +0330

    Fix: update admin.

--- ExchangeSystem/cosmos/transfer/admin.py
+++ ExchangeSystem/cosmos/transfer/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import SenderWallet, ReceiverWallet, CreateTransaction
+from .models import SenderWallet, ReceiverWallet, CreateTransaction, PublishTransaction
 
 
 admin.site.register(SenderWallet)
 admin.site.register(ReceiverWallet)
 admin.site.register(CreateTransaction)
+admin.site.register(PublishTransaction)
+
