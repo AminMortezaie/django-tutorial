8a0a15ed52d8c4179c1035e10bdca7728c884a28
mortezaei <mohammadmortezaie@gmail.com>
2023-02-22 21:07:40 +0330

    Fix: change db to postgres.

--- ExchangeSystem/cosmos/cosmos/settings.py
+++ ExchangeSystem/cosmos/cosmos/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'cosmos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'withdraw',
+        'USER': 'amin',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
