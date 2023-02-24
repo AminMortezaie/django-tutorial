a7ae5381e8bac4118ee19763189646ee1463fb52
mortezaei <mohammadmortezaie@gmail.com>
2023-02-25 00:52:57 +0330

    Fix: redis problem solved.

--- ExchangeSystem/cosmos-app/cosmos/settings.py
+++ ExchangeSystem/cosmos-app/cosmos/settings.py
@@ -90,11 +90,11 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/0',
+        'LOCATION': 'redis://redis:6379/0',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        },
-    },
+        }
+    }
 }
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
