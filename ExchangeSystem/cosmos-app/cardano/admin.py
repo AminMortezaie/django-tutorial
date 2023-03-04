01b7a0e125cf495278b3ecf0d6e26d9eff5ff0c5
mortezaei <mohammadmortezaie@gmail.com>
2023-03-04 14:58:59 +0330

    Fix: add cardano admin values.

--- ExchangeSystem/cosmos-app/cardano/admin.py
+++ ExchangeSystem/cosmos-app/cardano/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CardanoSenderWallet, CardanoReceiverWallet, CardanoCreateTransaction
 
-# Register your models here.
+
+admin.site.register(CardanoSenderWallet)
+admin.site.register(CardanoReceiverWallet)
+admin.site.register(CardanoCreateTransaction)
