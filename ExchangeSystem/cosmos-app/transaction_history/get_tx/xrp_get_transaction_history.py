ea35fc481feb29c13e4eed029a71d0595640a5a2
mortezaei <mohammadmortezaie@gmail.com>
2023-04-17 15:50:31 +0100

    Feat: add xrp to networks.

---
+++ ExchangeSystem/cosmos-app/transaction_history/get_tx/xrp_get_transaction_history.py
@@ -0,0 +1,25 @@
+import requests
+
+address = "rh4mXbGzMho3Vxwf8uwpHjBxZfkhnMZusS"
+
+
+def get_transactions_xrp(wallet_address):
+    responses = []
+    api_url = f'https://api.xrpscan.com/api/v1/account/{wallet_address}/transactions'
+    result = requests.get(api_url).json()['transactions']
+
+    for tx in result:
+        from_address = tx['Account']
+        to_address = tx['Destination']
+        amount = float(tx['Amount']['value'])/10**6
+        tx_hash = tx['hash']
+        if from_address == wallet_address:
+            responses.insert(0, {"tx": tx_hash, "type": "OUT", "amount": amount})
+        elif to_address == wallet_address:
+            responses.insert(0, {"tx": tx_hash, "type": "IN", "amount": amount})
+
+    return responses
+
+
+res = get_transactions_xrp(wallet_address=address)
+print(res)
